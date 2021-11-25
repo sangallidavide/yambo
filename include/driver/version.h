1fd4aa0458abdee630d9799044b50c525145ee52
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2021-11-25 14:26:27 +0100

    Version 5.1.0,  Revision 20496,  Hash 05a2b7adb
    MODIFIED *  include/driver/version.h io/io_ELPH.F modules/mod_ELPH.F ypp/el-ph/ELPH_databases.F ypp/interface/INIT_load_ypp.F ypp/interface/INIT_ypp.F
    Additions:
     -Added possibility to skip DW terms
    
    Changes:
    -
    
    Patch sent by:  Claudio Attaccalite <claudio.attaccalite@gmail.com>

--- include/driver/version.h
+++ include/driver/version.h
@@ -25,6 +25,6 @@
 #define YAMBO_VERSION 5
 #define YAMBO_SUBVERSION 1
 #define YAMBO_PATCHLEVEL 0
-#define YAMBO_REVISION 20495
-#define YAMBO_HASH "fce7a116d" 
+#define YAMBO_REVISION 20496
+#define YAMBO_HASH "05a2b7adb" 
 
