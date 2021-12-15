e3446dbad415e5fb2b580cd95c6595028f2e2eae
Davide Sangalli <davide.sangalli@gmail.com>
2021-12-15 18:09:27 +0100

    Version 5.1.0,  Revision 20541,  Hash 4c47f3ac8
    MODIFIED *  include/driver/version.h bse/K_Transitions_setup.F bse/K_dipoles.F bse/K_driver_init.F modules/SET_defaults.F modules/mod_electrons.F xc_functionals/XC_eval_lda_kernel.F
    
    Bugs:
    - [yambo] Fix for magnons when majority channel is spin-down
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- include/driver/version.h
+++ include/driver/version.h
@@ -25,6 +25,6 @@
 #define YAMBO_VERSION 5
 #define YAMBO_SUBVERSION 1
 #define YAMBO_PATCHLEVEL 0
-#define YAMBO_REVISION 20540
-#define YAMBO_HASH "f69cad8eb" 
+#define YAMBO_REVISION 20541
+#define YAMBO_HASH "4c47f3ac8" 
 
