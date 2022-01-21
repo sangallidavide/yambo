7ed6ebd5244d2594e038d47e1dd6a8508069371c
Davide Sangalli <davide.sangalli@gmail.com>
2022-01-21 11:46:12 +0100

    Version 5.1.0,  Revision 21157,  Hash d0a8fedb2
    MODIFIED *  include/driver/version.h ypp/real_time/RT_TRabs_driver.F ypp/real_time/RT_TRabs_residuals.F
    
    Changes:
    - [ypp_rt] few more fixes to transient absorption
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- include/driver/version.h
+++ include/driver/version.h
@@ -25,6 +25,6 @@
 #define YAMBO_VERSION 5
 #define YAMBO_SUBVERSION 1
 #define YAMBO_PATCHLEVEL 0
-#define YAMBO_REVISION 21156
-#define YAMBO_HASH "d9ba8be56" 
+#define YAMBO_REVISION 21157
+#define YAMBO_HASH "d0a8fedb2" 
 
