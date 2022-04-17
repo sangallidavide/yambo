be1c4a950ebcddef8caf539dfaf018d1f4831d76
Davide Sangalli <davide.sangalli@gmail.com>
2022-04-17 19:28:21 +0200

    Version 5.1.1,  Revision 21450,  Hash 2aca53a
    MODIFIED *  ypp/real_time/RT_TR_ARPES.F
    
    Changes:
    - [ypp_rt] TR ARPES, further improving to messages
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- include/driver/version.h
+++ include/driver/version.h
@@ -25,6 +25,6 @@
 #define YAMBO_VERSION 5
 #define YAMBO_SUBVERSION 1
 #define YAMBO_PATCHLEVEL 1
-#define YAMBO_REVISION 21449
-#define YAMBO_HASH "e73cca9" 
+#define YAMBO_REVISION 21450
+#define YAMBO_HASH "2aca53a" 
 
