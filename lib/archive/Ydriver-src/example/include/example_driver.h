9503c5d424c94c9d1933d15203741631f3ea9f5e
Aesku <ali.esuqembre@gmail.com>
2024-05-16 08:36:32 +0200

    Amplitude nosoc bug fixed

---
+++ lib/archive/Ydriver-src/example/include/example_driver.h
@@ -0,0 +1,8 @@
+/*
+  License-Identifier: GPL
+ 
+  Copyright (C) 2020 The Yambo Team
+ 
+  Authors (see AUTHORS file for details): AM
+*/
+void options_example(struct options_struct options[],int *i_opt);
